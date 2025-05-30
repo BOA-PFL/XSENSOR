760eb33c1c6ed68132047c4d9997eb104417ead9
kayrennBOA <kaleigh.renninger@boatechnology.com>
2025-05-30 16:06:44 -0600

    Update AnklePressure_Workwear.py

--- AnklePressure_Workwear.py
+++ AnklePressure_Workwear.py
@@ -31,7 +31,7 @@ plt.rc('ytick', labelsize=SMALL_SIZE)    # fontsize of the tick labels
 plt.rc('legend', fontsize=SMALL_SIZE)    # legend fontsize
 plt.rc('figure', titlesize=BIGGER_SIZE)  # fontsize of the figure title
 
-save_on = 0
+save_on = 1
 
 data_check = 0
 
