0b0957642c28093850e51ee4e19d820d4164fcff
EricHonertBOA <eric.honert@boatechnology.com>
2025-12-19 12:09:24 -0700

    Update CropTrials_on_Notes.py

--- CropTrials_on_Notes.py
+++ CropTrials_on_Notes.py
@@ -18,8 +18,8 @@ import math
 import matplotlib.pyplot as plt
 import os
 
-fPath = 'C:/Users/eric.honert/Boa Technology Inc/PFL Team - General/Testing Segments/WorkWear_Performance/EH_Workwear_MidCutStabilityI_CPDMech_June23/Pressure/Raw/' #Where the uncropped data live
-fSave = 'C:/Users/eric.honert/Boa Technology Inc/PFL Team - General/Testing Segments/WorkWear_Performance/EH_Workwear_MidCutStabilityI_CPDMech_June23/Pressure/' #Where to save cropped data
+fPath = 'C:/Users/eric.honert/Boa Technology Inc/PFL Team - General/PFL Internal Studies/ML_Pressure_COMPower/Pilot/XSENSOR/raw/' #Where the uncropped data live
+fSave = 'C:/Users/eric.honert/Boa Technology Inc/PFL Team - General/PFL Internal Studies/ML_Pressure_COMPower/Pilot/XSENSOR/' #Where to save cropped data
 fileExt = r".csv"
 entries = [fName for fName in os.listdir(fPath) if fName.endswith(fileExt)]
 cropped_entries = [fName for fName in os.listdir(fSave) if fName.endswith(fileExt)]
@@ -46,7 +46,7 @@ for entry in entries:
         for ii in idx:
             if ii-prev_note > 5: #Must have at least 5 frames between activies
                 print(Notes[ii])
-                portion = dat.iloc[prev_note:ii,:]
+                portion = dat.iloc[prev_note:ii+1,:]
                 if "\r" in Notes[ii]:
                     # If there is a space in the note name, remove it.
                     tmpTrial = Notes[ii].split('\r')[0].replace(" ","")
@@ -56,7 +56,7 @@ for entry in entries:
                 # Save the portion of the data
                 portion.to_csv(fSave+subject+'_'+tmpConfig+'_'+tmpTrial+'_'+tmpSesh+'.csv',index=False)
             
-            prev_note = ii
+            prev_note = ii+1
         
             
         
